561931232fa74c50ea73f66f3980ca6a89cfeea3
Eduardo Muchak <eduardomuchak@gmail.com>
2023-04-20 10:53:14 -0300

    fix: :bug: adjusts font loading warning

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,10 +1,9 @@
-import { ActivityIndicator, Text, View } from "react-native";
+import { ActivityIndicator, Text, View } from 'react-native';
 
 export function Loading() {
   return (
     <View className="h-screen items-center justify-center flex flex-col flex-1 gap-2">
       <ActivityIndicator color="#556AEB" size={30} />
-      <Text className="font-poppinsBold text-xl">Carregando</Text>
     </View>
   );
 }
