114bde2d22d892564c4cd950e31fb535f8e21637
Gabriel Peralta <prates_peralta@hotmail.com>
2023-04-20 15:11:52 +0000

    Configure/eslint

--- src/components/ui/Input/index.tsx
+++ src/components/ui/Input/index.tsx
@@ -7,9 +7,7 @@ interface Props extends TextInputProps {
 export function Input({ label, ...rest }: Props) {
   return (
     <>
-      <Text className="font-poppinsBold text-sm leading-4 text-neutral-900">
-        {label}
-      </Text>
+      <Text className="font-poppinsBold text-sm leading-4 text-neutral-900">{label}</Text>
       <TextInput
         className="bg-neutral-100 px-5 py-2 rounded-lg h-14 m-0 font-poppinsSemibold"
         {...rest}
