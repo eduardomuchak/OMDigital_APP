fb7d1adc97ae37dc008a24904cf9b6c22f08a4ee
gabriel <prates_peralta@hotmail.com>
2023-07-14 17:26:31 -0300

    refactor: change token name for encodedCredentials

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,13 +2,13 @@ import { API_BASE_URL, API_PASSWORD, API_USER } from '@env';
 import axios from 'axios';
 import { encode } from 'base-64';
 
-const token = encode(`${API_USER}:${API_PASSWORD}`);
+const encodedCredentials = encode(`${API_USER}:${API_PASSWORD}`);
 
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
     Accept: 'text/plain',
-    Authorization: `Basic ${token}`,
+    Authorization: `Basic ${encodedCredentials}`,
   },
 });
 
